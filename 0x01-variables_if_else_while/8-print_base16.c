c735f9fa29479cd1c8dc6c3337e4573a7b560949
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-02-22 07:04:43 -0800

    8 Print Base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-int main() {
-  // loop over all the numbers of base 16 from 0 to 15
-  for(int num = 0; num < 16; num++) {
-    // print the current number in hexadecimal using putchar
-    putchar(num < 10 ? num + 48 : num + 87);  // if num is less than 10, add the ASCII value of '0' to num to print the corresponding character; otherwise, add the ASCII value of 'a' - 10 to num to print the corresponding character
-    
-    // print a newline character using putchar after the 8th character to split the output into two lines
-    if(num == 7) {
-      putchar('\n');
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int num = 48;
+
+    while (num < 103)
+    {
+        if (num == 58)
+        {
+            num = 97;
+        }
+        putchar(num);
+        num++;
     }
-  }
-  
-  // print a newline character using putchar to move to a new line after printing all the numbers
-  putchar('\n');
-  
-  return 0;
+
+    putchar('\n');
+
+    return (0);
 }
 
