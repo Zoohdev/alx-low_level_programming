b9f1f05611db995f2c732e918155f0fcad4f7c0e
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-02-22 06:59:15 -0800

    9 Print Combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-int main() {
-  for(int i = 0; i < 10; i++) {
-    for(int j = i; j < 10; j++) {
-      putchar(i + 48);   // print the first number in the combination
-      putchar(',');      // print the comma separator
-      putchar(' ');      // print the space separator
-      putchar(j + 48);   // print the second number in the combination
-      if(j != 9) {       // if the second number is not 9, print the comma separator and space separator for the next combination
-        putchar(',');  
-        putchar(' ');
-      }
+int main(void)
+{
+    int i, j, k;
+
+    for (i = 0; i < 8; i++)
+    {
+        for (j = i + 1; j < 9; j++)
+        {
+            for (k = j + 1; k < 10; k++)
+            {
+                putchar(i + '0');
+                putchar(',');
+                putchar(' ');
+                putchar(j + '0');
+                putchar(',');
+                putchar(' ');
+                putchar(k + '0');
+                putchar(',');
+                putchar(' ');
+            }
+        }
     }
-  }
-  putchar('\n');        // print a newline character to move to a new line after printing all combinations
-  return 0;
+    putchar('\n');
+
+    return (0);
 }
 
