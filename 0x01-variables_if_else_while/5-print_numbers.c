42d3587c91fcc921322affb044500b6265a97ea3
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-02-22 05:18:48 -0800

    5 Print Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-int main() {
-  int num = 0;  // initialize a variable to hold the current number
-  
-  // loop over all single digit numbers from 0 to 9
-  while(num < 10) {
-    // print the current number using putchar
-    putchar(num + '0');  // add the ASCII value of '0' to num to print the corresponding character
-    num++;  // increment the number
-  }
-  
-  // print a newline character using putchar to move to a new line
-  putchar('\n');
-  
-  return 0;
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int digit = 0;
+
+        while (digit < 10)
+        {
+                putchar(digit + '0');
+                digit++;
+        }
+
+        putchar('\n');
+
+        return (0);
 }
 
