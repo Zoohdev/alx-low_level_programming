eb033dc4963f6b8a0949b4623ff78b3de981654f
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-02-22 06:54:52 -0800

    100 Print Combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 
-int main() {
-  for (int i = 0; i < 10; i++) {
-    for (int j = i + 1; j < 10; j++) {
-      putchar(i + '0');   // print the first digit of the combination
-      putchar(j + '0');   // print the second digit of the combination
-      if (i != 8 || j != 9) {  // if this is not the last combination, print the separator
-        putchar(',');          // print the comma separator
-        putchar(' ');          // print the space separator
-      }
-    }
-  }
-  putchar('\n');      // print a newline character to move to a new line after printing all combinations
-  return 0;
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int digit1 = 0;
+        int digit2;
+
+        while (digit1 < 9)
+        {
+                digit2 = digit1 + 1;
+                while (digit2 < 10)
+                {
+                        putchar(digit1 % 10 + '0');
+                        putchar(digit2 % 10 + '0');
+                        if (digit1 != 8 || digit2 != 9)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                        digit2++;
+                }
+                digit1++;
+        }
+        putchar('\n');
+
+        return (0);
 }
 
