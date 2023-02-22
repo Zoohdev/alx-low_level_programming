f31a759de3f5fde12df18f937a8901657c4e8879
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-02-22 05:35:06 -0800

    6 Print Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-int main() {
-  // loop over all single digit numbers from 0 to 9
-  for(int num = 0; num < 10; num++) {
-    // print the current number using putchar
-    putchar(num + 48);  // add the ASCII value of '0' (which is 48) to num to print the corresponding character
-  }
-  
-  // print a newline character using putchar to move to a new line
-  putchar(10);  // add the ASCII value of '\n' (which is 10) to print the newline character
-  
-  return 0;
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int digit = 0;
+
+        while (digit < 10)
+        {
+                putchar(digit / 10 + '0');
+                putchar(digit % 10 + '0');
+                digit++;
+        }
+
+        putchar('\n');
+
+        return (0);
 }
 
