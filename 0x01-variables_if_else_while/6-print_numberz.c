cc8409613bbaf24dcf14a52703dec70b8bda0d73
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-04-21 07:21:06 -0700

    6 Print Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,11 @@
  */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++) /* loop through single-digit numbers */
-    {
-        putchar(i + 48); /* print the number as a character */
-    }
-
-    putchar('\n'); /* print newline character */
-
-    return (0);
+int i;
+for (i = 0; i < 10; i++) /* loop through single-digit numbers */
+{
+putchar(i + 48); /* print the number as a character */
 }
-
-
+putchar('\n'); /* print newline character */
+return (0);
+}
