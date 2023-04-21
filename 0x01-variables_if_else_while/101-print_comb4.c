48ba4b348267a72d2fcc1a52140a85ef5086d84b
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-04-21 07:54:36 -0700

    101 Print Combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,34 +6,32 @@
  */
 int main(void)
 {
-        int digit1 = 0;
-        int digit2;
-        int digit3;
+int digit1 = 0;
+int digit2;
+int digit3;
 
-        while (digit1 < 8)
-        {
-                digit2 = digit1 + 1;
-                while (digit2 < 9)
-                {
-                        digit3 = digit2 + 1;
-                        while (digit3 < 10)
-                        {
-                                putchar(digit1 % 10 + '0');
-                                putchar(digit2 % 10 + '0');
-                                putchar(digit3 % 10 + '0');
-                                if (digit1 != 7 || digit2 != 8 || digit3 != 9)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                                digit3++;
-                        }
-                        digit2++;
-                }
-                digit1++;
-        }
-        putchar('\n');
-
-        return (0);
+while (digit1 < 8)
+{
+digit2 = digit1 + 1;
+while (digit2 < 9)
+{
+digit3 = digit2 + 1;
+while (digit3 < 10)
+{
+putchar(digit1 % 10 + '0');
+putchar(digit2 % 10 + '0');
+putchar(digit3 % 10 + '0');
+if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+{
+putchar(',');
+putchar(' ');
 }
-
+digit3++;
+}
+digit2++;
+}
+digit1++;
+}
+putchar('\n');
+return (0);
+}
