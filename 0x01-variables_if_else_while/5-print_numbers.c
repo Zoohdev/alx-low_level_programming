b0253dc0f3c66ce95969d0ccae2a5e0b9020ff06
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-04-21 06:58:31 -0700

    Print Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,16 +6,13 @@
  */
 int main(void)
 {
-        int digit = 0;
+int digit = 0;
 
-        while (digit < 10)
-        {
-                putchar(digit + '0');
-                digit++;
-        }
-
-        putchar('\n');
-
-        return (0);
+while (digit < 10)
+{
+putchar(digit + '0');
+digit++;
 }
-
+putchar('\n');
+return (0);
+}
