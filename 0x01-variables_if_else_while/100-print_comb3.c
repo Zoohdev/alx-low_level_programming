fb4884f9a8bd996d56f8241a29e85cf9b9f7329a
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-04-21 07:49:24 -0700

    100 Print Combs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,27 +6,25 @@
  */
 int main(void)
 {
-        int digit1 = 0;
-        int digit2;
+int digit1 = 0;
+int digit2;
 
-        while (digit1 < 9)
-        {
-                digit2 = digit1 + 1;
-                while (digit2 < 10)
-                {
-                        putchar(digit1 % 10 + '0');
-                        putchar(digit2 % 10 + '0');
-                        if (digit1 != 8 || digit2 != 9)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                        digit2++;
-                }
-                digit1++;
-        }
-        putchar('\n');
-
-        return (0);
+while (digit1 < 9)
+{
+digit2 = digit1 + 1;
+while (digit2 < 10)
+{
+putchar(digit1 % 10 + '0');
+putchar(digit2 % 10 + '0');
+if (digit1 != 8 || digit2 != 9)
+{
+putchar(',');
+putchar(' ');
 }
-
+digit2++;
+}
+digit1++;
+}
+putchar('\n');
+return (0);
+}
