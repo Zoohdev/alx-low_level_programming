ba947160475137818721cfe78de0c7b63dc51590
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-04-24 23:25:13 -0700

    2 Print Alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,21 +5,16 @@
  *                      followed by a new line after each iteration.
  */
 void print_alphabet_x10(void)
-{
-        char letter;
-        int i;
+{	char letter;
+int i;
 
-        for (i = 0; i < 10; i++)
-        {
-                letter = 'a';
-
-                while (letter <= 'z')
-                {
-                        _putchar(letter);
-                        letter++;
-                }
-
-                _putchar('\n');
-        }
+for (i = 0; i < 10; i++)
+{	letter = 'a';
+while (letter <= 'z')
+{	_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
 }
 
